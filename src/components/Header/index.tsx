0002527e06b5aafc460619d2dc540dd5fcf757dc
Marcos Santos <marcosadriano034@gmail.com>
2021-10-02 17:13:48 -0300

    feat: initial transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 import moneyImg from '../../assets/logo.svg'
 import { Container, Content } from './styles';
 
 const Header: React.FC = () => {
+  const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
+
+
+  const handleOpenNewTrasactionModalOpen = () => {
+    setIsNewTrasactionModalOpen(true)
+  }
+
+  const handleOpenNewTrasactionModalClose = () => {
+    setIsNewTrasactionModalOpen(false)
+  }
+
   return (
     <Container>
       <Content>
         <img src={moneyImg} alt="finance controll" />
-        <button>Nova transação</button>
+        <button onClick={handleOpenNewTrasactionModalOpen}>Nova transação</button>
+        <Modal
+          isOpen={isNewTrasactionModalOpen}
+          onRequestClose={handleOpenNewTrasactionModalClose}
+        >
+          <h2>Cadastrar transação</h2>
+        </Modal>
       </Content>
     </Container>
   );
