a26188b4534b4517d4a8f94531d0cfe0e869718b
Marcos Santos <marcosadriano034@gmail.com>
2021-10-02 18:16:21 -0300

    styles: add buttons of the transaction types

--- src/components/NewTrasactionModal/index.tsx
+++ src/components/NewTrasactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal'
-import { Container } from './styles';
-
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypesContatiner } from './styles';
 interface NewTrasactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -17,10 +19,23 @@ const NewTrasactionModal = ({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button type="button" onClick={onRequestClose} className="react-modal-close" >
+        <img src={closeImg} alt="fechar modal" />
+      </button>
       <Container>
         <h2>Cadastrar transação</h2>
         <input type="text" placeholder="Título" />
         <input type="number" placeholder="Valor" />
+        <TransactionTypesContatiner>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypesContatiner>
         <input type="text" placeholder="Categoria" />
         <button type="submit">
           Cadastrar
